aaf84d677e113e0eb3a597bf9c4db892f863eb71
pramodaragay5 <pramodaragay@gmail.com>
2026-08-04 21:34:20 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+        int count = 0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0)+1);
+        }
+
+        for (int i=0; i<t.length(); i++){
+            char ch = t.charAt(i);
+
+            if(!hm.containsKey(ch)) return false;
+
+            hm.put(ch, hm.get(ch)-1);
+
+            if(hm.get(ch) < 0) return false;
+        }
+            return true;
+    }
+}
